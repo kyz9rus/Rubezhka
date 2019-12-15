fed742785ad9d57ffc3e4239eacc2e75f9cabf64
maximus1998g <maximus1998g@mail.ru>
2019-12-15 20:14:52 +0300

    task 5

--- task5/main.cpp
+++ task5/main.cpp
@@ -1,21 +1,28 @@
 #include <iostream>
+#include <unistd.h>
 #include "ThreadPool.h"
 #include "assert.h"
 
 using namespace std;
 
-#define THREADS_COUNT 8
-#define MAX 99
+#define THREADS_COUNT 7
+#define MAX 40
 int arr1[MAX];
 int arr2[MAX];
+int temp[MAX];
+int temp_curr_index = 0;
 
 typedef struct Interval {
     int low;
     int high;
 } Interval;
 
-pthread_mutex_t m_mutex;
-atomic_int current_point;
+pthread_mutex_t bounds_mutex;
+pthread_mutex_t temp_mutex;
+
+int current_point;
+int thread_counter = 1;
+ThreadPool pool(THREADS_COUNT);
 
 void merge(int a[], int low, int mid, int high)
 {
@@ -61,21 +68,52 @@ void merge_sort(int a[], int low, int high)
     }
 }
 
-void merge_sort_threads(int a[])
+Interval init_bounds()
 {
-    pthread_mutex_lock(&m_mutex);
 
-    int local_current_point = current_point.load();
+    int local_current_point = current_point;//.load();
     int low, high;
 
-    low = local_current_point;
-    if(local_current_point < MAX - MAX / THREADS_COUNT - MAX % THREADS_COUNT)
-        high = local_current_point + MAX / THREADS_COUNT - 1;
+    low = current_point;
+
+//    if(local_current_point < MAX - MAX / THREADS_COUNT - MAX % THREADS_COUNT)
+//        high = local_current_point + MAX / THREADS_COUNT - 1;
+//    else
+//        high = MAX - 1;
+
+    if(thread_counter < THREADS_COUNT)
+        high = (thread_counter * MAX / THREADS_COUNT) - 1;
     else
         high = MAX - 1;
+    cout << low << " " << high << "\n";
+
 
     current_point = high + 1;
-    cout << low << " " << high << "\n";
+
+    Interval *interval;
+    interval = (Interval *) calloc(1, sizeof(Interval));
+    interval->low = low;
+    interval->high = high;
+
+
+
+    return *interval;
+}
+
+void merge_sort_threads(int a[])
+{
+
+    // int local_thread_counter = thread_counter++;
+
+
+    pthread_mutex_lock(&bounds_mutex);
+    Interval interval = init_bounds();
+    pthread_mutex_unlock(&bounds_mutex);
+
+
+    int low = interval.low,
+        high = interval.high;
+
 
     int mid = low + (high - low) / 2;
     if (low < high) {
@@ -84,14 +122,42 @@ void merge_sort_threads(int a[])
         merge(a, low, mid, high);
     }
 
-    pthread_mutex_unlock(&m_mutex);
+    // fill in temp array after a thread has sorted smth
+    pthread_mutex_lock(&temp_mutex);
+    thread_counter++;
+
+    int local_temp_curr_index = temp_curr_index;
+    for(int i = low; i <= high; i++){
+        temp[temp_curr_index] = a[i];
+        temp_curr_index++;
+    }
+
+
+    if(thread_counter >= 2){
+        // pool.add_task(merge_sort_threads);
+        // первый поток отработал
+        // второй поток отработал -> merge(1, 2)
+        // третий поток отработал -> merge((1, 2), 3)
+        // четвертый поток отработал -> merge(((1, 2), 3), 4)
+        // пятый поток отработал -> merge((((1, 2), 3), 4), 5)
+        merge(temp, 0, temp_curr_index - MAX / THREADS_COUNT - 1, temp_curr_index - 1);
+
+    }
+//    for(int i = 0; i < temp_curr_index; i++)
+//        cout << temp[i] << " ";
+//    cout << "\n";
+
+
+    pthread_mutex_unlock(&temp_mutex);
+
+
 }
 
 
 int main() {
-    pthread_mutex_init(&m_mutex, nullptr);
+    pthread_mutex_init(&bounds_mutex, nullptr);
+    pthread_mutex_init(&temp_mutex, nullptr);
 
-    ThreadPool pool(THREADS_COUNT);
     pool.init();
 
 
@@ -112,7 +178,7 @@ int main() {
         for (int i = 0; i < THREADS_COUNT; ++i) {
             pool.add_task(merge_sort_threads, arr1);
         }
-        pool.shutdown();
+
 
 //        merge(arr1, 0, (MAX / 2 - 1) / 2, MAX / 2 - 1);
 //        merge(arr1, MAX / 2, MAX/2 + (MAX-1-MAX/2)/2, MAX - 1);
@@ -126,16 +192,24 @@ int main() {
 
     }
 
+    sleep(0);
+    pool.shutdown();
 
-    for(int i = 0; i < MAX; i++){
-        if(i % (MAX / THREADS_COUNT) == 0)
-            cout << "\n";
-        cout << "[" << i << "]" << ":" << arr1[i] << " ";
-    }
+    sleep(0);
 
-//    for (int i = 1; i < MAX; i++) {
-//        assert(arr1[i - 1] <= arr1[i]);
+//    for(int i = 0; i < MAX; i++){
+//        if(i % (MAX / THREADS_COUNT) == 0)
+//            cout << "\n";
+//        cout << "[" << i << "]" << ":" << temp[i] << " ";
 //    }
+    cout << "\n";
+
+    for (int i = 0; i < sizeof(temp) / sizeof(temp[0]); i++) {
+        // cout << i << "\n";
+        if(temp[i] > temp[i + 1])
+            cout << "чорт";
+        // assert(temp[i] <= temp[i + 1]);
+    }
 
 
 
