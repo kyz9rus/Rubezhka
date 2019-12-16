01d0b4e8ffe9d408a34ef488f6bb7adb34807d49
maximus1998g <maximus1998g@mail.ru>
2019-12-16 23:15:47 +0300

    task 5

--- task5/main.cpp
+++ task5/main.cpp
@@ -1,33 +1,29 @@
 #include <iostream>
-#include <unistd.h>
 #include "ThreadPool.h"
-#include "assert.h"
+#include <mutex>
 
 using namespace std;
 
-#define THREADS_COUNT 7
-#define MAX 40
-int arr1[MAX];
-int arr2[MAX];
+#define THREADS_COUNT 1
+#define MAX 2000000
 int temp[MAX];
 int temp_curr_index = 0;
+
+int current_point;
+int thread_counter = 1;
+ThreadPool my_pool(THREADS_COUNT);
+
+mutex bounds_mutex;
+mutex temp_mutex;
 
 typedef struct Interval {
     int low;
     int high;
 } Interval;
 
-pthread_mutex_t bounds_mutex;
-pthread_mutex_t temp_mutex;
-
-int current_point;
-int thread_counter = 1;
-ThreadPool pool(THREADS_COUNT);
-
-void merge(int a[], int low, int mid, int high)
-{
-    int* left = new int[mid - low + 1];
-    int* right = new int[high - mid];
+void merge(int a[], int low, int mid, int high) {
+    int left[mid - low + 1];
+    int right[high - mid];
 
     int n1 = mid - low + 1,
         n2 = high - mid, i, j;
@@ -57,8 +53,7 @@ void merge(int a[], int low, int mid, int high)
     }
 }
 
-void merge_sort(int a[], int low, int high)
-{
+void merge_sort(int a[], int low, int high) {
     int mid = low + (high - low) / 2;
     if (low < high) {
         merge_sort(a, low, mid);
@@ -68,52 +63,67 @@ void merge_sort(int a[], int low, int high)
     }
 }
 
-Interval init_bounds()
-{
-
-    int local_current_point = current_point;//.load();
+Interval init_bounds() {
     int low, high;
 
     low = current_point;
-
-//    if(local_current_point < MAX - MAX / THREADS_COUNT - MAX % THREADS_COUNT)
-//        high = local_current_point + MAX / THREADS_COUNT - 1;
-//    else
-//        high = MAX - 1;
-
     if(thread_counter < THREADS_COUNT)
         high = (thread_counter * MAX / THREADS_COUNT) - 1;
     else
         high = MAX - 1;
-    cout << low << " " << high << "\n";
-
-
     current_point = high + 1;
+
+    thread_counter++;
+    // cout << low << " " << high << endl;
 
     Interval *interval;
     interval = (Interval *) calloc(1, sizeof(Interval));
     interval->low = low;
     interval->high = high;
 
-
-
     return *interval;
 }
 
-void merge_sort_threads(int a[])
-{
+void is_sorted(int arr[]) {
+    int errors = 0;
+    int ids[10];
 
-    // int local_thread_counter = thread_counter++;
+    for(int & id : ids)
+        id = 0;
 
+    for (int i = 0; i < MAX - 1; i++)
+        if (arr[i] > arr[i + 1]){
+            ids[errors] = i;
+            errors++;
+        }
 
-    pthread_mutex_lock(&bounds_mutex);
+    if (errors != 0) {
+        cout << "\nслучилось что-то не очень хорошее...(" << endl;
+        cout << "errors: " << errors << endl << endl;
+
+        for(int i = 0; i < sizeof(ids) / sizeof(ids[0]); i++){
+            if(ids[i] != 0){
+                for(int j = -10; j < 10; j++){
+                    if(j == i){
+                        cout << ">>> [" << ids[i] + j << "]: " << arr[ids[i] + j] << endl;
+                    }
+                    else{
+                        cout << "[" << ids[i] + j << "]: " << arr[ids[i] + j] << endl;
+                    }
+                }
+                cout << endl;
+            }
+        }
+    }
+}
+
+void merge_sort_threads(int a[]) {
+    bounds_mutex.lock();
     Interval interval = init_bounds();
-    pthread_mutex_unlock(&bounds_mutex);
-
+    bounds_mutex.unlock();
 
     int low = interval.low,
         high = interval.high;
-
 
     int mid = low + (high - low) / 2;
     if (low < high) {
@@ -122,112 +132,62 @@ void merge_sort_threads(int a[])
         merge(a, low, mid, high);
     }
 
-    // fill in temp array after a thread has sorted smth
-    pthread_mutex_lock(&temp_mutex);
-    thread_counter++;
-
-    int local_temp_curr_index = temp_curr_index;
+    // merge parts
+    temp_mutex.lock();
+    int added = 0;
     for(int i = low; i <= high; i++){
         temp[temp_curr_index] = a[i];
         temp_curr_index++;
+        added++;
     }
+    int local_temp_curr_index = temp_curr_index;
+    int new_mid = local_temp_curr_index - added;
 
-
-    if(thread_counter >= 2){
-        // pool.add_task(merge_sort_threads);
-        // первый поток отработал
-        // второй поток отработал -> merge(1, 2)
-        // третий поток отработал -> merge((1, 2), 3)
-        // четвертый поток отработал -> merge(((1, 2), 3), 4)
-        // пятый поток отработал -> merge((((1, 2), 3), 4), 5)
-        merge(temp, 0, temp_curr_index - MAX / THREADS_COUNT - 1, temp_curr_index - 1);
-
-    }
-//    for(int i = 0; i < temp_curr_index; i++)
-//        cout << temp[i] << " ";
-//    cout << "\n";
-
-
-    pthread_mutex_unlock(&temp_mutex);
-
+    merge(temp, 0, new_mid - 1, local_temp_curr_index - 1);
+    temp_mutex.unlock();
 
 }
 
-
-int main() {
-    pthread_mutex_init(&bounds_mutex, nullptr);
-    pthread_mutex_init(&temp_mutex, nullptr);
-
-    pool.init();
-
-
+int* init_arr(int arr[]) {
     for(int i = 0; i < MAX; i++){
         int num = rand() % MAX;
-        arr1[i] = num;
-        //arr2[i] = num;
+        arr[i] = num;
     }
 
-//    for(int i : arr1)
-//        cout << i << " ";
-//
-//    cout << "\n";
+    return arr;
+}
 
-    {
-        //auto start = std::chrono::system_clock::now();
-
-        for (int i = 0; i < THREADS_COUNT; ++i) {
-            pool.add_task(merge_sort_threads, arr1);
-        }
-
-
-//        merge(arr1, 0, (MAX / 2 - 1) / 2, MAX / 2 - 1);
-//        merge(arr1, MAX / 2, MAX/2 + (MAX-1-MAX/2)/2, MAX - 1);
-//        merge(arr1, 0, (MAX - 1)/2, MAX - 1);
-//
-//        auto end = std::chrono::system_clock::now();
-//        std::chrono::duration<double> elapsed_seconds = end - start;
-//        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-//        std::cout << "finished computation at " << std::ctime(&end_time)
-//                  << "elapsed time: " << elapsed_seconds.count() << "s\n";
-
+void very_smart_merge_sort(int arr[]){
+    for (int i = 0; i < THREADS_COUNT; ++i) {
+        my_pool.add_task(merge_sort_threads, arr);
     }
 
-    sleep(0);
-    pool.shutdown();
+    my_pool.shutdown();
+}
 
-    sleep(0);
+void show_computation_time(int arr[]){
+    auto start = chrono::system_clock::now();
 
-//    for(int i = 0; i < MAX; i++){
-//        if(i % (MAX / THREADS_COUNT) == 0)
-//            cout << "\n";
-//        cout << "[" << i << "]" << ":" << temp[i] << " ";
-//    }
-    cout << "\n";
+    very_smart_merge_sort(arr);
 
-    for (int i = 0; i < sizeof(temp) / sizeof(temp[0]); i++) {
-        // cout << i << "\n";
-        if(temp[i] > temp[i + 1])
-            cout << "чорт";
-        // assert(temp[i] <= temp[i + 1]);
-    }
+    auto end = chrono::system_clock::now();
+    chrono::duration<double> elapsed_seconds = end - start;
+    cout << "computation time: " << elapsed_seconds.count() << endl;
+}
 
+void print_arr(){
+    for(int i : temp)
+        cout << i << " ";
+}
 
+int main() {
+    my_pool.init();
 
-    {
-     //   auto start = std::chrono::system_clock::now();
+    int arr[MAX];
+    init_arr(arr);
 
-        //merge_sort(arr2, 0, MAX);
-//
-//        auto end = std::chrono::system_clock::now();
-//        std::chrono::duration<double> elapsed_seconds = end - start;
-//        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
-//        std::cout << "elapsed time: " << elapsed_seconds.count() << "s\n";
-
-    }
-
-//    for (size_t i = 1; i < sizeof(arr2) / sizeof(arr2[0]); i++) {
-//        assert(arr2[i - 1] <= arr2[i]);
-//    }
+    show_computation_time(arr);
+    is_sorted(temp);
 
     return 0;
 }
